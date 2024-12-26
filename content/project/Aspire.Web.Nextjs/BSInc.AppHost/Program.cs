65b10e8b4a95398155375317d3175452ccfe9830
Brian Sheridan <brian.sheridan@gmail.com>
2024-12-25 17:58:07 -0800

    Update package references to version 9.0.0 and clean up project files

--- content/project/Aspire.Web.Nextjs/BSInc.AppHost/Program.cs
+++ content/project/Aspire.Web.Nextjs/BSInc.AppHost/Program.cs
@@ -7,7 +7,7 @@ var postgresdb = postgres.AddDatabase("postgresdb");
 
 var apiService = builder.AddProject<Projects.BSInc_API>("apiservice");
 
-var nextjs = builder.AddNpmApp("nextjs","../BSInc.Web")
+var nextjs = builder.AddNpmApp("nextjs", "../BSInc.Web")
                     .WithReference(apiService)
                     .WithEnvironment("BROWSER", "none")
                     .WithHttpEndpoint(env: "PORT")
@@ -15,10 +15,4 @@ var nextjs = builder.AddNpmApp("nextjs","../BSInc.Web")
                     .PublishAsDockerFile();
 
 
-
-/*
-builder.AddProject<Projects.AspireTemplate_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
-*/
 builder.Build().Run();
