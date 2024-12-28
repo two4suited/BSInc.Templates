3ae9f4bedb0eba5b70fdbb30bb7b404cfdedbc47
Brian Sheridan <brian.sheridan@gmail.com>
2024-12-28 11:49:10 -0800

    Add service bus configuration and database setup for Aspire.Messaging

--- content/project/Aspire.Messaging/BSInc.AppHost/Program.cs
+++ content/project/Aspire.Messaging/BSInc.AppHost/Program.cs
@@ -2,16 +2,19 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var saPassword = builder.AddParameter("password",secret:true);
 var port = 1433;
+var serviebusConfigfilePath = Path.Combine(Directory.GetCurrentDirectory(), "config.json");;
 
-var sql = builder.AddSqlServer("sql", saPassword,port)
-    .WithImage("azure-sql-edge")
-    .WithLifetime(ContainerLifetime.Persistent);
+var sql = builder.AddSqlServer("sql", saPassword, port)
+    .WithImage("azure-sql-edge");
+   
 
 
 var db = sql.AddDatabase("database");
 
 var servicebus = builder.AddContainer("servicebus", "mcr.microsoft.com/azure-messaging/servicebus-emulator")
     .WithReference(sql)
+    .WaitFor(sql)
+    .WithVolume($"{serviebusConfigfilePath}:/ServiceBus_Emulator/ConfigFiles/Config.json")
     .WithEnvironment("MSSQL_SA_PASSWORD", sql.Resource.PasswordParameter.Value)
     .WithEnvironment("SQL_SERVER", "sql")
     .WithEnvironment("ACCEPT_EULA", "Y");
